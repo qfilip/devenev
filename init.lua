3de6858d7a3967d50e31913ca35811e2c9b3d709
qfilip <kukolj.filip@gmail.com>
2024-03-27 21:14:39 +0100

    neovim config

---
+++ init.lua
@@ -0,0 +1,4 @@
+require("phq.options")
+require("phq.keymaps")
+-- require("phq.lazy")
+require("phq.paq")
---
+++ lua/phq/paq.lua
@@ -0,0 +1,6 @@
+require "paq" {
+    "savq/paq-nvim", -- Let Paq manage itself
+    "neovim/nvim-lspconfig",
+    { "lervag/vimtex", opt = true }, -- Use braces when passing options
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+}
