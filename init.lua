39c767b47cd4cbcdf57dfb7d7b09f3bcb12c9459
qfilip <kukolj.filip@gmail.com>
2024-03-27 21:31:49 +0100

    removed nvim config from master branch

--- init.lua
+++
@@ -1,4 +0,0 @@
-require("phq.options")
-require("phq.keymaps")
--- require("phq.lazy")
-require("phq.paq")
--- lua/phq/paq.lua
+++
@@ -1,8 +0,0 @@
-require "paq" {
-    "savq/paq-nvim", -- Let Paq manage itself
-    "neovim/nvim-lspconfig",
-    { "lervag/vimtex", opt = true }, -- Use braces when passing options
-    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-    { 'nvim-lua/plenary.nvim' },
-    { 'nvim-telescope/telescope.nvim', tag = '0.1.6' }
-}
