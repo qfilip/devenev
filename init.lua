535da481f167988e0add825508464c62d65d33d5
qfilip <kukolj.filip@gmail.com>
2025-09-12 18:56:17 +0200

    updated config

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("phq.options")
 require("phq.keymaps")
 require("phq.lazynvim")
+require("phq.paq")
--- lua/phq/paq.lua
+++ lua/phq/paq.lua
@@ -4,5 +4,6 @@ require "paq" {
     { "lervag/vimtex", opt = true }, -- Use braces when passing options
     { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
     { 'nvim-lua/plenary.nvim' },
-    { 'nvim-telescope/telescope.nvim', tag = '0.1.6', build = ':TSInstall! lua' }
+    { 'nvim-telescope/telescope.nvim', tag = '0.1.6', build = ':TSInstall! lua' },
+    { 'mason-org/mason.nvim' }
 }
